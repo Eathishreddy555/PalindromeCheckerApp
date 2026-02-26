08d3a00ef93467f164f46b85303ebe364a1d72ce
EATHISH REDDY KOTHAPATI <ek2673@srmist.edu.in>
2026-02-26 15:03:55 +0530

    UC2: Print a Hardcoded Palindrome Result

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,18 @@
 public class PalindromeCheckerApp {
-    public static void main(){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
+    public static void main() {
+        String word = "madam";
+        boolean isPalindrome = true;
+        int n = word.length()-1;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != word.charAt(n - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println(word+ " is a Palindrome");
+        }else{
+            System.out.println(word+ " is not a palindrome");
+        }
     }
 }
